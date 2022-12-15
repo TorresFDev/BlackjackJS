bb9212c479bf3278482111cbf1b6d02d5b2a55d2
FacundoTorres <torresfdev@gmail.com>
2022-12-15 17:17:08 -0300

    se volvio a enlazar html

--- index.js
+++ index.js
@@ -25,5 +25,5 @@ document.getElementById("form").addEventListener("submit", (e) => {
 
     localStorage.setItem("datosJugador", JSON.stringify(datosJugador));
     datosInicio.remove();
-    window.location.href = "./BlackjackJS/main.html";
+    window.location.href = "./main.html";
 });
